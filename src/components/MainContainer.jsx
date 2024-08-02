1570dae5f427d869f6a5df771b7f9edbcb0e9b48
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-08-02 15:53:00 +0530

    FEAT: Creates main container.

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 120px;
+  min-height: 100vh;
+`;
+export default MainContainer;
