9a7dec0a54b04b8f505c5ff271d3b90d1c3b675d
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-08-02 15:48:49 +0530

    REFACTOR: Change component name and add padding property and font-size property.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,12 +7,12 @@ const StyledButton = styled(Link)`
   justify-content: center;
   align-items: center;
   gap: 6px;
-  padding: 0.8em 1em;
+  padding: 12px 15px;
   background-color: black;
   border-radius: 6px;
   border: none;
   color: #fff;
-  font-size: 16px;
+  font-size: 1em;
   text-decoration: none;
   &:hover {
     color: #000;
@@ -23,10 +23,11 @@ const StyledButton = styled(Link)`
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 1.2em;
   }
 `;
 
-function Button({ text, icon, route }) {
+function LinkButton({ text, icon, route }) {
   return (
     <StyledButton to={route}>
       {text ? text : ''}
@@ -35,10 +36,10 @@ function Button({ text, icon, route }) {
   );
 }
 
-Button.propTypes = {
+LinkButton.propTypes = {
   text: PropTypes.string,
   icon: PropTypes.element,
   route: PropTypes.string,
 };
 
-export default Button;
+export default LinkButton;
