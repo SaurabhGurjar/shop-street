d76bf6b2b778ce4f590118b1a19f4256727a284b
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-08-02 15:50:39 +0530

    REFACTOR: Add white logo.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,12 @@
-import logo from '../../public/logo.png';
-export default function Logo() {
-  return <img src={logo} />;
+import PropTypes from 'prop-types';
+import logo from '/logo.png';
+import logoWhite from '/logo-white.png';
+function Logo({ isWhite = false }) {
+  return <img src={isWhite ? logoWhite : logo} />;
 }
+
+Logo.propTypes = {
+  isWhite: PropTypes.bool,
+};
+
+export default Logo;
